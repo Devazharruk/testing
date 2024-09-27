4b332ab2b3811e22ef8c7d9cf7dc490fa3af8623
Azhar Ali <149366630+Devazharruk@users.noreply.github.com>
2024-09-27 13:40:28 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:5173"],
+    origin: ["http://localhost:3000", "http://localhost:5173","https://testing-front-brown.vercel.app"],
     credentials: true,
   })
 );
